2c71e5b912ef039494c41b13ae0015728512b475
Sachi Goto <70562492+SachiGoto@users.noreply.github.com>
2022-10-08 22:32:58 -0700

    added environment variable and hosted database on heroku

--- client/src/app/common-service/service.service.ts
+++ client/src/app/common-service/service.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
-  private url = "http://localhost:3000";
-    constructor(private http:HttpClient) { }
 
+
+    constructor(private http:HttpClient) { }
+    // private url = environment.server;
+    private url = environment.server;
     getTasks(){
       return this.http.get(this.url);
 
